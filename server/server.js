826bd2ea37243165e8ce798092cfaa604b58351b
vamshi <thipireddyvamshikiranreddy@gmail.com>
2026-03-26 11:15:53 +0530

    added root route

--- server/server.js
+++ server/server.js
@@ -10,6 +10,9 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("🚀 MeetAhead Backend is Running!");
+});
 
 // ✅ MongoDB
 mongoose.connect(
